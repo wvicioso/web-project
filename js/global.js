92e9c0b0cacc69b4c38ce5cb5493ab89cf2a305e
wvicioso <wellington.vicioso@gmail.com>
2019-05-01 01:41:21 -0400

    finalize first 2 pages

--- js/global.js
+++ js/global.js
@@ -1,8 +1,5 @@
 function fadeIn(i=1) {
   var bodyElems = document.body.children;
-  console.log(bodyElems)
-  console.log(bodyElems.length)
-  console.log(i)
 
   setTimeout(function(){
     bodyElems[i].style.opacity = 1;
@@ -13,6 +10,26 @@ function fadeIn(i=1) {
   }, 500)
 }
 
+function slideInRightSide() {
+  var elem = document.getElementsByClassName('right-side')[0];
+  setTimeout(function(){
+    if (elem) {
+      elem.style.right = '0';
+    }
+  }, 800)
+}
+
+function slideAboutHeader() {
+  var elem = document.getElementsByClassName('101')[0];
+  setTimeout(function(){
+    if (elem) {
+      elem.style.marginLeft = '50px';
+    }
+  }, 500)
+}
+
 window.addEventListener("load", function(){
   fadeIn();
+  slideInRightSide();
+  slideAboutHeader();
 });

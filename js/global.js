68456f62dd6b5b0abfdaed5d7a43071ec4713b92
wvicioso <wellington.vicioso@gmail.com>
2019-05-06 15:55:51 -0400

    finish website

--- js/global.js
+++ js/global.js
@@ -42,6 +42,53 @@ function closeMenu() {
   elem.style.marginLeft = '-80%';
 }
 
+function ValidateEmail(mail)
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
+  {
+    return (true)
+  }
+    // alert("You have entered an invalid email address!")
+    return (false)
+}
+
+function validateForm(e) {
+  e.preventDefault();
+  var name = document.getElementById('Name')
+  var email = document.getElementById('Email')
+  var inquiry = document.getElementById('Inquiry')
+  var counter = 0;
+  if (name.value == "") {
+    document.getElementById('nameError').style.display = "inline-block";
+  } else {
+    document.getElementById('nameError').style.display = "none";
+    counter++;
+  }
+
+  if (!ValidateEmail(email) || email.value == "" ) {
+    document.getElementById('emailError').style.display = "inline-block";
+  } else {
+    document.getElementById('emailError').style.display = "none";
+    counter++;
+  }
+
+  if (inquiry.value == "") {
+    document.getElementById('inquiryError').style.display = "inline-block";
+  } else {
+    document.getElementById('inquiryError').style.display = "none";
+    counter++;
+  }
+
+  if(counter == 3) {
+    document.getElementById('form').style.display = 'none'
+    document.getElementById('contact-p').style.display = 'none'
+    document.getElementById('thankyou').style.display = 'inline-block'
+  }
+
+
+  return false;
+}
+
 window.addEventListener("load", function(){
   setWidth();
   fadeIn();
